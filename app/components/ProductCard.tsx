752163642a7cc2e532d299d0d551bd4a68453ff7
Tarashdeep Singh <92468703+tarashdeepsingh@users.noreply.github.com>
2024-10-13 18:38:22 +0530

    Add files via upload

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProductCard = () => {
+  return (
+    <AddToCart />
+  )
+}
+
+export default ProductCard
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <Link href="/users" className="flex">Users</Link>
+      <ProductCard />
+    </main>
+  );
+}
